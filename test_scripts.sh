bfe931fd3e46e7ac25b1eca7989a24fcc0007403
Athindran Ramesh Kumar <arkumar@eespml1.ee.princeton.edu>
2023-05-16 02:13:52 -0400

    Running more tests and sanity checks

--- test_scripts.sh
+++ test_scripts.sh
@@ -3,5 +3,7 @@ python test_yaw_constraints.py -cf ./test_configs/reachavoid/test_config_cbf_rea
 python test_yaw_constraints.py -cf ./test_configs/reachavoid/test_config_cbf_reachavoid_circle_config1.yaml -pt reachavoidcf1_lqr_task
 python test_yaw_constraints.py -cf ./test_configs/reachavoid/test_config_cbf_reachavoid_circle_config1.yaml -pt reachavoidcf1_lqr_task_squeeze -rb 1.0
 python test_yaw_constraints.py -cf ./test_configs/reachavoid/test_config_cbf_reachavoid_circle_config2.yaml -pt reachavoidcf2_lqr_task
+python test_yaw_constraints.py -cf ./test_configs/reachavoid/test_config_cbf_reachavoid_circle_config2.yaml --naive_task -pt reachavoidcf2_naive_task
 python test_yaw_constraints.py -cf ./test_configs/reachavoid/test_config_cbf_reachavoid_circle_config3.yaml -pt reachavoidcf3_lqr_task
+python test_yaw_constraints.py -cf ./test_configs/reachavoid/test_config_cbf_reachavoid_circle_config3.yaml --naive_task -pt reachavoidcf3_naive_task
 python test_yaw_constraints.py -cf ./test_configs/reachavoid/test_config_cbf_reachavoid_circle_config4.yaml -pt reachavoidcf4_lqr_task
