21038c927bf677c32558ab2473e5ec941c4096db
Athindran Ramesh Kumar <arkumar@della8.princeton.edu>
2023-07-24 16:22:33 -0400

    Adding an 0.5 to the dynamics jacobian

--- simulators/dynamics/bicycle5d.py
+++ simulators/dynamics/bicycle5d.py
@@ -110,7 +110,7 @@ class Bicycle5D(BaseDynamics):
                       [0, 0, jnp.tan(obs[4])/self.wheelbase, 0, obs[2]/(1e-6 + self.wheelbase*jnp.cos(obs[4])**2)],
                       [0, 0, 0, 0, 0]  ])
       
-      Ad = jnp.eye(self.dim_x) + Ac*self.dt +Ac@Ac*self.dt*self.dt
+      Ad = jnp.eye(self.dim_x) + Ac*self.dt + 0.5*Ac@Ac*self.dt*self.dt
         
       return Ad
   
@@ -155,7 +155,7 @@ class Bicycle5D(BaseDynamics):
                       [0, 0],
                       [0, 1]])
       
-      Ad = jnp.eye(self.dim_x) + Ac*self.dt +Ac@Ac*self.dt*self.dt
+      Ad = jnp.eye(self.dim_x) + Ac*self.dt + 0.5*Ac@Ac*self.dt*self.dt
       Bd = self.dt * Bc
 
       return Ad, Bd
